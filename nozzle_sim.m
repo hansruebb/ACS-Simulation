3c736fa0a17a150894ded09fbd3b4bfec47b3c24
Hans Rübberdt <hans.ruebberdt@stud.tu-darmstadt.de>
2024-03-26 17:33:30 +0100

    some changes

--- nozzle_sim.m
+++ nozzle_sim.m
@@ -1,42 +1,44 @@
 u = symunit;
 
-for r = 0.35:0.05:0.75
-    for e = 5:0.2:7
-        throat_radius = r * u.mm;
-        expansion_ratio = e;
+load('constants.mat')
+
+r_vec = 0.02:0.02:0.4; %0.05:0.05:1.0;
+e_vec = 1.5:0.5:8;
+res = zeros(size(r_vec, 2), size(e_vec, 2));
+
+for r_idx = 1:size(r_vec, 2)
+    for e_idx = 1:size(e_vec, 2)
+        throat_radius = r_vec(r_idx) * u.mm;
+        expansion_ratio = e_vec(e_idx);
         
-        throat_area_raw_mm = double(getValue(unitConvert(throat_radius, u.mm)^2 * pi));
+        THROAT_AREA_SQMM = double(getValue(unitConvert(throat_radius, u.mm)^2 * pi));
         [tp, isp] = run_sim(throat_radius, expansion_ratio);
-        disp(['The specific impuls for radius ', num2str(r), 'mm and expansion ratio ', num2str(e) , ' is: ', num2str(isp), ' s'])
-        disp(['The total impuls for radius ', num2str(r), 'mm and expansion ratio ', num2str(e) , ' is: ', num2str(tp), ' Ns'])
+        res(r_idx, e_idx) = isp;
+        disp(['The specific impuls for radius ', num2str(r_vec(r_idx)), 'mm and expansion ratio ', num2str(e_vec(e_idx)) , ' is: ', num2str(isp), ' s'])
+        disp(['The total impuls for radius ', num2str(r_vec(r_idx)), 'mm and expansion ratio ', num2str(e_vec(e_idx)) , ' is: ', num2str(tp), ' Ns'])
     end 
 end
+
+surf(r_vec, e_vec, res');
+xlabel("Throat radius (mm)");
+ylabel("Expansion ratio");
+zlabel("Specific impuls (s)");
 
 function [total_impuls, specific_impuls] = run_sim(throat_radius, expansion_ratio)
 
     u = symunit;
-    
-    %Outside pressure, gas constant and specific heat ratio are hard-coded
-    %here
-    outside_pressure = 1 * (u.bar);
-    
+
     simlog_acs_simulation = sim("acs_simulation").simlog_acs_simulation;
 
-    time_steps = simlog_acs_simulation.Pressure_Regulator.mdot_B.series.time;
-    gas_constant = simscape.Value(ones(length(time_steps), 1) * 287.05, symunit2str(u.J/ (u.kg * u.K), 'Simscape'));
-    specific_heat_ratio = simscape.Value(ones(length(time_steps), 1) * 1.401);
-    mass_flow = simscape.Value(simlog_acs_simulation.Pressure_Regulator.mdot_B.series.values, simlog_acs_simulation.Pressure_Regulator.mdot_B.series.unit);
-    init_temp = simscape.Value(simlog_acs_simulation.Pressure_Regulator.B.T.series.values, simlog_acs_simulation.Pressure_Regulator.B.T.series.unit);
-    init_pressure = simscape.Value(simlog_acs_simulation.Pressure_Regulator.B.p.series.values, simlog_acs_simulation.Pressure_Regulator.B.p.series.unit);
-    
-    r = gas_constant;
-    gamma = specific_heat_ratio;
-    m_dot = -mass_flow;
-    t_t = init_temp;
-    p_t = init_pressure;
-    r_t = simscape.Value(ones(length(time_steps), 1) * double(getValue(throat_radius)), symunit2str(getUnit(throat_radius)));
-    ratio = simscape.Value(ones(length(time_steps), 1) * double(expansion_ratio));
-    p_0 = simscape.Value(ones(length(time_steps), 1) * double(getValue(outside_pressure)), symunit2str(getUnit(outside_pressure)));
+    time_steps = simlog_acs_simulation.Nozzle.mdot_A.series.time;                                                                   % Simulation time steps
+    r = simscape.Value(ones(length(time_steps), 1) * 287.05, symunit2str(u.J/ (u.kg * u.K), 'Simscape'));                           % Gas constant (hard-coded for now)
+    gamma = simscape.Value(ones(length(time_steps), 1) * 1.401);                                                                    % Specific heat ratio (hard-coded for now)
+    m_dot = simscape.Value(simlog_acs_simulation.Nozzle.mdot_A.series.values, simlog_acs_simulation.Nozzle.mdot_A.series.unit);     % Input mass flow
+    t_t = simscape.Value(simlog_acs_simulation.Nozzle.A.T.series.values, simlog_acs_simulation.Nozzle.A.T.series.unit);             % Input temperature
+    p_t = simscape.Value(simlog_acs_simulation.Nozzle.A.p.series.values, simlog_acs_simulation.Nozzle.A.p.series.unit);             % Input pressure
+    ratio = simscape.Value(ones(length(time_steps), 1) * double(expansion_ratio));                                                  % Expansion ratio
+    p_0 = simscape.Value(ones(length(time_steps), 1) * 1.0, symunit2str(u.bar, 'Simscape'));                                        % Atmospheric pressure (hard-coded for now)
+    r_t = simscape.Value(ones(length(time_steps), 1) * double(getValue(throat_radius)), symunit2str(getUnit(throat_radius)));       % Throat radius
     
     %Calculate recommended throat size
     %a_star_recommended = ((m_dot .* sqrt(t_t)) ./ p_t ) .* ( 1 ./ (sqrt(gamma ./ r) .* power(((gamma + 1) ./ 2), -(gamma + 1) ./ (2 .* (gamma - 1))))); 
@@ -48,7 +50,7 @@ function [total_impuls, specific_impuls] = run_sim(throat_radius, expansion_rati
     syms M_e;
     %TODO Calculate time dependent
     g = double(gamma(1));
-    exit_mach = vpasolve(expansion_ratio == ((g + 1) / 2)^(-(g + 1) / (2 * (g - 1))) * ((1 + (((g - 1) / 2) * M_e^2))^((g + 1) / (2 * (g - 1))) / M_e), M_e);
+    exit_mach = vpasolve(expansion_ratio == ((g + 1) / 2)^(-(g + 1) / (2 * (g - 1))) * ((1 + (((g - 1) / 2) * M_e^2))^((g + 1) / (2 * (g - 1))) / M_e), M_e, 3.0);
     exit_mach = simscape.Value(ones(length(time_steps), 1) * double(exit_mach));
     t_e = (1 + ((gamma - 1) / 2) .* exit_mach.^2).^(-ones(length(time_steps), 1)) .* t_t;
     p_e = (1 + ((gamma - 1) / 2) .* exit_mach.^2).^(-gamma ./ (gamma - 1)) .* p_t;
@@ -62,6 +64,8 @@ function [total_impuls, specific_impuls] = run_sim(throat_radius, expansion_rati
     %title("Thrust over time")
     %xlabel("Time (s)")
     %ylabel("Thrust (N)")
+
+    %disp(['The exit mach is: ', num2str(exit_mach(1).value)])
     
     F = griddedInterpolant(time_steps, value(thrust));
     f_t = @(t) (F(t));
